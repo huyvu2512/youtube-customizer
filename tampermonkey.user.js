2b75442632606a8ddfdaea8045e0259b0fe9423d
Huy Vũ <quanghuy25121995@gmail.com>
2026-04-12 22:31:32 +0700

    Add files via upload

--- tampermonkey.user.js
+++ tampermonkey.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         YouTube Customizer
 // @namespace    http://tampermonkey.net/
-// @version      1.3
-// @description  Tùy biến giao diện (Logo Premium/4 cột video) và nâng cao trải nghiệm xem video (Tua Numpad/Skip Ads).
+// @version      1.4
+// @description  Tùy biến giao diện (Logo Premium/4 cột video) và nâng cao trải nghiệm (Tua Numpad/Skip Ads/Auto-Continue).
 // @author       Huy Vũ
 // @require      https://raw.githubusercontent.com/huyvu2512/youtube-customizer/main/youtube_customizer.js
 // @match        https://www.youtube.com/*
